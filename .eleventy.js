d2597ae8da00ac6fff901b55e0073cbca630ff7a
Jonathan Snook <jonathan@snook.ca>
2023-08-10 16:47:25 -0400

    Add RSS Feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const { DateTime } = require("luxon");
 const Image = require("@11ty/eleventy-img");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addFilter('prettyDate', (dateObj) => {
     var d = DateTime.fromJSDate(dateObj, {zone:'utc'});
     return d.toFormat("LLLL d, kkkk");
